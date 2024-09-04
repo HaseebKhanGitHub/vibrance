5d0fd32633ec8907b7b5065ff79f4ddd38e40943
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-04 17:05:09 +0500

    update

--- lib/Views/Auth/loginScreen.dart
+++ lib/Views/Auth/loginScreen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class login extends StatelessWidget {
   const login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vibrance',
+            style: GoogleFonts.poppins(
+                fontWeight: FontWeight.bold, color: Colors.white)),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  children: [
+                    Text('Sign in to Vibrance',
+                        style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
