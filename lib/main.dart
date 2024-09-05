a5d20680f27a0fe18ec7bee6a2a38a77b976bced
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-05 21:08:21 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vibrance/Views/Auth/loginScreen.dart';
+import 'package:vibrance/Views/Invitation/inviteFriend.dart';
 import 'package:vibrance/Views/Profile/profileSetup.dart';
 
 import 'Views/Auth/forgotPasswordScreen.dart';
@@ -18,6 +19,6 @@ class vibrance extends StatelessWidget {
     return MaterialApp(
         title: 'Vibrance',
         debugShowCheckedModeBanner: false,
-        home: profileSetup());
+        home: inviteFriend());
   }
 }
