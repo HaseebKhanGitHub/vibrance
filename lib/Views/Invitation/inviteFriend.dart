830ac2f909f9ae3f6db10a50c76a5e89a0910484
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-13 22:07:35 +0500

    Update inviteFriend.dart

--- lib/Views/Invitation/inviteFriend.dart
+++ lib/Views/Invitation/inviteFriend.dart
@@ -50,214 +50,217 @@ class inviteFriend extends StatelessWidget {
               )
             ],
           )),
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            height: 50,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.black26,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.black26,
+              ),
+              child: Text(
+                'Find Your Friend',
+                style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18.sp,
+                    color: Colors.black,
+                    letterSpacing: 2),
+              ),
             ),
-            child: Text(
-              'Find Your Friend',
-              style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18.sp,
-                  color: Colors.black,
-                  letterSpacing: 2),
+            SizedBox(
+              height: 30,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              children: [
-                Expanded(
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                        //width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20)),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  spreadRadius: 3,
+                                  blurRadius: 10)
+                            ]),
+                        child: TextField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15),
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: 'Hi Faizan, Type an exact username',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0)),
+                              hintStyle:
+                                  TextStyle(fontSize: 15, color: Colors.grey),
+                            ))),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 3,
+                              blurRadius: 10)
+                        ]),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            //-----------------------------------------------------
+            /*Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Card(
+                  elevation: 10.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  color: Colors.white,
                   child: Container(
-                      //width: 300,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 3,
-                                blurRadius: 10)
-                          ]),
-                      child: TextField(
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 15),
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: 'Hi Faizan, Type an exact username',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0)),
-                            hintStyle:
-                                TextStyle(fontSize: 15, color: Colors.grey),
-                          ))),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.orange,
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 3,
-                            blurRadius: 10)
-                      ]),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          //-----------------------------------------------------
-          /*Container(
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Card(
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
-                color: Colors.white,
-                child: Container(
-                  height: 100,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 20, 5, 15),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.purple,
-                              radius: 30,
+                    height: 100,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 20, 5, 15),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.purple,
+                                radius: 30,
+                              ),
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    bottom: 5, left: 10),
-                                child: Text(
-                                  '@Haseeb5741',
-                                  style: TextStyle(fontSize: 17),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 14),
-                                child: Text(
-                                  'Haseeb',
-                                  style: TextStyle(
-                                      color: Colors.grey, fontSize: 13),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Spacer(),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 5),
-                                  child: GFButton(
-                                    onPressed: () {},
-                                    color: Colors.cyan,
-                                    shape: GFButtonShape.pills,
-                                    child: Text(
-                                      'Accept',
-                                      style: TextStyle(fontSize: 10),
-                                    ),
+                                  padding: const EdgeInsets.only(
+                                      bottom: 5, left: 10),
+                                  child: Text(
+                                    '@Haseeb5741',
+                                    style: TextStyle(fontSize: 17),
                                   ),
                                 ),
                                 Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 5),
-                                  child: GFButton(
-                                    onPressed: () {},
-                                    color: Colors.red.shade500,
-                                    shape: GFButtonShape.pills,
-                                    child: Text(
-                                      'Decline',
-                                      style: TextStyle(fontSize: 10),
-                                    ),
+                                  padding: const EdgeInsets.only(left: 14),
+                                  child: Text(
+                                    'Haseeb',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 13),
                                   ),
                                 ),
                               ],
                             ),
-                          )
-                        ],
-                      )
-                    ],
+                            Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 5),
+                                    child: GFButton(
+                                      onPressed: () {},
+                                      color: Colors.cyan,
+                                      shape: GFButtonShape.pills,
+                                      child: Text(
+                                        'Accept',
+                                        style: TextStyle(fontSize: 10),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 5),
+                                    child: GFButton(
+                                      onPressed: () {},
+                                      color: Colors.red.shade500,
+                                      shape: GFButtonShape.pills,
+                                      child: Text(
+                                        'Decline',
+                                        style: TextStyle(fontSize: 10),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
+            ),*/
+            SizedBox(
+              height: 15,
             ),
-          ),*/
-          SizedBox(
-            height: 15,
-          ),
-          Container(
-              width: 400,
-              height: 300,
-              child: Image.asset(
-                "assets/images/notitfication.jpg",
-                filterQuality: FilterQuality.high,
-                fit: BoxFit.cover,
-              )),
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            'Search for your friends on Vibrance or\n invite your friend to Vibrance',
-            style: GoogleFonts.nunito(
-                color: Colors.grey.shade500,
-                fontWeight: FontWeight.bold,
-                fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Button action
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.purple,
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(20), // Set the rounded corners here
+            Container(
+                width: 400,
+                height: 300,
+                child: Image.asset(
+                  "assets/images/notitfication.jpg",
+                  filterQuality: FilterQuality.high,
+                  fit: BoxFit.cover,
+                )),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Search for your friends on Vibrance or\n invite your friend to Vibrance',
+              style: GoogleFonts.nunito(
+                  color: Colors.grey.shade500,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Button action
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      20), // Set the rounded corners here
+                ),
               ),
-            ),
-            child: Text(
-              'Invite a Friend',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
+              child: Text(
+                'Invite a Friend',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
