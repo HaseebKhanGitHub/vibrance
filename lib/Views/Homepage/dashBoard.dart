8299e447e414c892330c5e5e845da7bec4b0e5da
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-21 00:01:26 +0500

    commit

--- lib/Views/Homepage/dashBoard.dart
+++ lib/Views/Homepage/dashBoard.dart
@@ -7,15 +7,51 @@ class dashBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        SizedBox(
+          height: 10,
+        ),
         Container(
           child: Text(
             'Dashboard',
             textAlign: TextAlign.center,
             style: TextStyle(
-                fontSize: 32,
+                fontSize: 25,
                 color: Colors.black,
                 fontWeight: FontWeight.w700,
                 letterSpacing: 2),
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          padding: EdgeInsets.only(left: 15),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Your Friend',
+            style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 1),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          width: double.infinity,
+          child: Card(
+            shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(20)),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [Container(), Container()],
+                  )
+                ],
+              ),
+            ),
           ),
         )
       ],
