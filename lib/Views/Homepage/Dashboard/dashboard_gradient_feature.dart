26db6633c17420647eb600fbef2dd3b117e3d5d3
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-27 00:00:04 +0500

    commit

--- lib/Views/Homepage/Dashboard/dashboard_gradient_feature.dart
+++ lib/Views/Homepage/Dashboard/dashboard_gradient_feature.dart
@@ -1,43 +1,50 @@
 import 'package:flutter/material.dart';
 
 class dashboard_gradient_feature extends StatelessWidget {
-  const dashboard_gradient_feature({super.key});
+  final IconData icon;
+  final String text;
+  final List<Color> clr;
+
+  dashboard_gradient_feature(this.icon, this.text, this.clr);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 50,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(15),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0.0, 3.0),
-            blurRadius: 3.0,
-          )
-        ],
-        gradient: LinearGradient(
-          colors: [Color(0xff4FC174), Color(0xff00D7A9)],
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone_android),
-            Flexible(
-                child: Text(
-              'Device Info',
-              style: TextStyle(
-                fontWeight: FontWeight.w300,
-              ),
-            ))
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0.0, 3.0),
+              blurRadius: 3.0,
+            )
           ],
+          gradient: LinearGradient(
+            colors: [Color(0xff4FC174), Color(0xff00D7A9)],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(icon),
+              Flexible(
+                  child: Text(
+                'Device Info',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
