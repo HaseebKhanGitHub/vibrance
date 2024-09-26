0e732b7565ad1cadf5f63c364021042ad5c16b20
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-26 23:48:45 +0500

    Update dashBoard.dart

--- lib/Views/Homepage/dashBoard.dart
+++ lib/Views/Homepage/dashBoard.dart
@@ -201,13 +201,53 @@ class dashBoard extends StatelessWidget {
                       ),
                       SizedBox(width: 15),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
           ),
-        )
+        ),
+        dashboard_gradient_feature()
       ],
+    );
+  }
+
+  Container dashboard_gradient_feature() {
+    return Container(
+      alignment: Alignment.center,
+      height: 50,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0.0, 3.0),
+            blurRadius: 3.0,
+          )
+        ],
+        gradient: LinearGradient(
+          colors: [Color(0xff4FC174), Color(0xff00D7A9)],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone_android),
+            Flexible(
+                child: Text(
+              'Device Info',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
