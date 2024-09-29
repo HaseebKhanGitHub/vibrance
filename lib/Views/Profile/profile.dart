26f90197d97c69308de412031aade5510e829790
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-29 23:24:23 +0500

    Update profile.dart

--- lib/Views/Profile/profile.dart
+++ lib/Views/Profile/profile.dart
@@ -1,3 +1,4 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class profile extends StatelessWidget {
@@ -15,12 +16,101 @@ class profile extends StatelessWidget {
               color: Colors.black),
         ),
         Container(
+          // height: 150,
           alignment: Alignment.center,
           decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage(''), fit: BoxFit.fill)),
+            image: DecorationImage(
+                image: AssetImage('assets/images/doodle.jpg'),
+                fit: BoxFit.fill),
+          ),
+          child: CircularProfileAvatar(
+            "",
+            backgroundColor: Colors.cyan,
+            borderWidth: 1,
+            borderColor: Colors.purpleAccent,
+            elevation: 20,
+            radius: 40,
+            cacheImage: true,
+            errorWidget: (context, url, error) {
+              return Icon(
+                Icons.face,
+                size: 50,
+              );
+            },
+            onTap: () {},
+            animateFromOldImageOnUrlChange: true,
+            placeHolder: (context, url) {
+              return Container(
+                  child: Center(child: CircularProgressIndicator()));
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'Jason Blake',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 10),
+        Text(
+          '@jay45',
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            // Button action
+          },
+          style: ElevatedButton.styleFrom(
+            side: BorderSide(color: Color(0xff671AFC)),
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(20), // Set the rounded corners here
+            ),
+          ),
+          child: Text(
+            'Edit Profile Details',
+            style: TextStyle(
+                color: Color(0xff671AFC),
+                fontSize: 14,
+                fontWeight: FontWeight.w600),
+          ),
         )
       ],
+    );
+  }
+}
+
+class cardDetailRow extends StatelessWidget {
+  cardDetailRow(this.type, this.icon, this.val, this.clr);
+
+  final String type;
+  final Icon icon;
+  final String val;
+  final Color clr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        child: Container(
+          decoration: BoxDecoration(
+              color: clr,
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          height: 50,
+          width: double.infinity,
+          child: Row(
+            children: [
+              SizedBox(width: 10),
+              icon,
+              Text(
+                type,
+                textScaler: TextScaler.linear(1.2),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
