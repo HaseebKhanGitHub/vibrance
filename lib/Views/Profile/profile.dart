a21bb53e541e8c36e698052b0166c6496bd77cf5
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-09-29 22:24:29 +0500

    commit

--- lib/Views/Profile/profile.dart
+++ lib/Views/Profile/profile.dart
@@ -1,8 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SizedBox(height: 15),
+        Text(
+          'My Profile',
+          style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 25,
+              letterSpacing: 2,
+              color: Colors.black),
+        ),
+        Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage(''), fit: BoxFit.fill)),
+        )
+      ],
+    );
   }
 }
