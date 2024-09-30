8884afd74d02238534f3a9ae17a8c18ca4b3cf46
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-01 00:14:50 +0500

    commit

--- lib/Views/Profile/profile.dart
+++ lib/Views/Profile/profile.dart
@@ -4,78 +4,102 @@ import 'package:flutter/material.dart';
 class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 15),
-        Text(
-          'My Profile',
-          style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 25,
-              letterSpacing: 2,
-              color: Colors.black),
-        ),
-        Container(
-          // height: 150,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/doodle.jpg'),
-                fit: BoxFit.fill),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 15),
+          Text(
+            'My Profile',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 25,
+                letterSpacing: 2,
+                color: Colors.black),
           ),
-          child: CircularProfileAvatar(
-            "",
-            backgroundColor: Colors.cyan,
-            borderWidth: 1,
-            borderColor: Colors.purpleAccent,
-            elevation: 20,
-            radius: 40,
-            cacheImage: true,
-            errorWidget: (context, url, error) {
-              return Icon(
-                Icons.face,
-                size: 50,
-              );
-            },
-            onTap: () {},
-            animateFromOldImageOnUrlChange: true,
-            placeHolder: (context, url) {
-              return Container(
-                  child: Center(child: CircularProgressIndicator()));
-            },
-          ),
-        ),
-        SizedBox(height: 15),
-        Text(
-          'Jason Blake',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-        ),
-        SizedBox(height: 10),
-        Text(
-          '@jay45',
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            // Button action
-          },
-          style: ElevatedButton.styleFrom(
-            side: BorderSide(color: Color(0xff671AFC)),
-            shape: RoundedRectangleBorder(
-              borderRadius:
-                  BorderRadius.circular(20), // Set the rounded corners here
+          Container(
+            // height: 150,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/doodle.jpg'),
+                  fit: BoxFit.fill),
+            ),
+            child: CircularProfileAvatar(
+              "",
+              backgroundColor: Colors.cyan,
+              borderWidth: 1,
+              borderColor: Colors.purpleAccent,
+              elevation: 20,
+              radius: 40,
+              cacheImage: true,
+              errorWidget: (context, url, error) {
+                return Icon(
+                  Icons.face,
+                  size: 50,
+                );
+              },
+              onTap: () {},
+              animateFromOldImageOnUrlChange: true,
+              placeHolder: (context, url) {
+                return Container(
+                    child: Center(child: CircularProgressIndicator()));
+              },
             ),
           ),
-          child: Text(
-            'Edit Profile Details',
-            style: TextStyle(
-                color: Color(0xff671AFC),
-                fontSize: 14,
-                fontWeight: FontWeight.w600),
+          SizedBox(height: 15),
+          Text(
+            'Jason Blake',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
-        )
-      ],
+          SizedBox(height: 10),
+          Text(
+            '@jay45',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Button action
+            },
+            style: ElevatedButton.styleFrom(
+              side: BorderSide(color: Color(0xff671AFC)),
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(20), // Set the rounded corners here
+              ),
+            ),
+            child: Text(
+              'Edit Profile Details',
+              style: TextStyle(
+                  color: Color(0xff671AFC),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                cardDetailRow(" Email", Icons.alternate_email,
+                    "jay45@gmail.com", Colors.grey.shade200),
+                cardDetailRow(" Country", Icons.outlined_flag,
+                    "United States", Colors.grey.shade200),
+                cardDetailRow(" Phone Number", Icons.phone,
+                    "Not Currently Set", Colors.grey.shade200),
+                cardDetailRow(
+                    " Gender", Icons.people, "Male", Colors.grey.shade200),
+                cardDetailRow(" Partner", Icons.person, "Robbie Williamss",
+                    Colors.grey.shade200),
+                cardDetailRow(" UID", Icons.fingerprint, "HUE83VFNU73BF65",
+                    Colors.grey.shade200),
+                cardDetailRow(" Account Created", Icons.timer_outlined,
+                    "22-09-2024", Colors.grey.shade200),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -84,7 +108,7 @@ class cardDetailRow extends StatelessWidget {
   cardDetailRow(this.type, this.icon, this.val, this.clr);
 
   final String type;
-  final Icon icon;
+  final IconData icon;
   final String val;
   final Color clr;
 
@@ -102,7 +126,7 @@ class cardDetailRow extends StatelessWidget {
           child: Row(
             children: [
               SizedBox(width: 10),
-              icon,
+              Icon(icon),
               Text(
                 type,
                 textScaler: TextScaler.linear(1.2),
@@ -116,7 +140,9 @@ class cardDetailRow extends StatelessWidget {
                       val,
                       textAlign: TextAlign.right,
                       style: TextStyle(
-                          color: val == "Not Currently set"
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          color: val == "Not Currently Set"
                               ? Colors.red
                               : Colors.black),
                     ),
