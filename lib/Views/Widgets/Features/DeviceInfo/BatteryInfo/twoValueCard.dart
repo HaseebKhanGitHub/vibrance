4f74847ebc83e266d16f1b2ae5be699c5f78a673
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-16 14:07:13 +0500

    commit

---
+++ lib/Views/Widgets/Features/DeviceInfo/BatteryInfo/twoValueCard.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class twoValueCard extends StatelessWidget {
+  final String text;
+  final String value;
+  final Widget? subWidget;
+  final Color? clr;
+  final Color? textclr;
+
+  twoValueCard(
+      {required this.text,
+      required this.value,
+      this.subWidget,
+      this.clr,
+      this.textclr});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      width: double.infinity,
+      child: Card(
+        elevation: 10,
+        color: clr ?? Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.w500),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 5),
+            subWidget ??
+                Text(
+                  value,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: textclr ?? Color(0xff23a8e1),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900),
+                )
+          ],
+        ),
+      ),
+    );
+  }
+}
