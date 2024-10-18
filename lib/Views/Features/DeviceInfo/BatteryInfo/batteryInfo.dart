f53334c854f7e8b90283a17b9673ac3321e693f1
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-18 23:49:12 +0500

    commit

---
+++ lib/Views/Features/DeviceInfo/BatteryInfo/batteryInfo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class batteryInfo extends StatelessWidget {
+  const batteryInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
