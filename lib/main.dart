6c13a2cc00e72a217ee23fcfe1ab9a3b6ef7d0af
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-18 23:49:18 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:vibrance/Views/Auth/loginScreen.dart';
+import 'package:vibrance/Views/Features/DeviceInfo/BatteryInfo/batteryInfo.dart';
 import 'package:vibrance/Views/Features/DeviceInfo/UserStatus/userStatus.dart';
 import 'package:vibrance/Views/Features/DeviceInfo/deviceInfo.dart';
 import 'package:vibrance/Views/Homepage/homePage.dart';
@@ -26,7 +27,7 @@ class vibrance extends StatelessWidget {
       return MaterialApp(
           title: 'Vibrance',
           debugShowCheckedModeBanner: false,
-          home: userStatus());
+          home: batteryInfo());
     });
   }
 }
